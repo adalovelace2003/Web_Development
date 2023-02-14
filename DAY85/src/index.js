af55a6e32d7ab9688aa656c2e2a0f4d204b0b943
adalovelace2003 <adalovelace2003@github.com>
2023-02-14 22:29:43 +0545

    JS Basics

---
+++ DAY85/src/index.js
@@ -0,0 +1,34 @@
+// TypeCasting
+
+a = 1
+b = "2"
+c = "a"
+
+// console.log(a+b) // ->12
+// console.log(a + Number(b)) // -> 3
+// console.log(Number(c))  // -> NAN
+
+//Type Coersion
+// console.log("I am " + 29 + " years old")
+
+// console.log("23" - "10" -3) 
+
+// console.log("23" * 2)
+
+
+// TRUTHY AND FALSY VALUES
+
+// Diff between == and === 
+// == does type coersion
+
+
+// const age = 18;
+// 
+// if(age ===18 ) console.log("You just became an adult")  //strict
+// 
+// if(age == '18' ) console.log(" You just become an adult")
+// 
+
+const fav = prompt("What's your facourite number");
+
+console.log(fav)
